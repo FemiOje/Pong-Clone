84e340aa5a57e66fbf8b5dd8e5eea73f1764ec07
Femi Ojetokun <ojetokunakinwale@gmail.com>
2023-10-19 03:20:50 +0100

    added UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,16 +5,32 @@ public class GameManager : MonoBehaviour
     public Ball ball;
     public int playerScore;
     public int computerScore;
+    public TMPro.TextMeshProUGUI playerScoreText;
+    public TMPro.TextMeshProUGUI computerScoreText;
+    public GameObject playerPaddle;
+    public GameObject computerPaddle;
 
     public void PlayerScored()
     {
         playerScore++;
+        playerScoreText.text = $"{playerScore}";
         this.ball.ResetPosition();
+        this.ball.AddStartingForce();
+        ResetPaddlePositions();
     }
 
     public void ComputerScored()
     {
         computerScore++;
+        computerScoreText.text = $"{computerScore}";
         this.ball.ResetPosition();
+        this.ball.AddStartingForce();
+        ResetPaddlePositions();
+    }
+
+    public void ResetPaddlePositions()
+    {
+        playerPaddle.transform.position = new Vector2(playerPaddle.transform.position.x, 0.0f);
+        computerPaddle.transform.position = new Vector2(computerPaddle.transform.position.x, 0.0f);
     }
 }
